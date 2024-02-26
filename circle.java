0b82f0ca6406036c03126935864888066865fcfb
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-02-26 14:54:01 -0500

    circle made
    idk

---
+++ circle.java
@@ -0,0 +1,13 @@
+public class circle {
+    public static void main(String[]args){
+        double radius = 4.27783;
+        double area = Math.PI*(radius*radius);
+        double circumference = Math.PI*(2*radius);
+        System.out.print("The area of the circle is ");
+        System.out.println(area);
+        System.out.print("The circumference of the circle is ");
+        System.out.println(circumference);
+    }
+//The area of the circle is 57.519776
+//The circumference of the circle is 26.8784
+}
