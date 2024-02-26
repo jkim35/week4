d06de3e54bb70af29f970fa365c1f12859b2a015
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-02-26 14:36:33 -0500

    rectangle made
    eyah

---
+++ Rect.java
@@ -0,0 +1,5 @@
+public class Rect{
+    public static void main(String[]args){
+        System.out.println("***************\n*\t      *\n*\t      *\n*\t      *\n*\t      *\n*\t      *\n***************");
+    }
+}
