aaf494a35103e7639ae1e383554783bb6134e8d2
Team6911 <155586579+jkim35@users.noreply.github.com>
2024-03-01 14:45:51 -0500

    hi guys
    fg

--- TempConverter.java
+++ TempConverter.java
@@ -6,7 +6,7 @@ public class TempConverter {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the temperature in Fahrenheit: ");
         temp = input.nextInt();
-        outTemp = ((temp - 32)*5)/9;
+        outTemp = ((temp - 32.0)*(5.0/9.0));
         System.out.println("The temperature in Celcius is "+ outTemp );
     }
 }
---
+++ numberFormat.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of class NumberFormatExample here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.text.NumberFormat;
+public class numberFormat{
+   
+
+       public static void main(String[] args){
+            double dollars = 21.5;
+            int num = 1234;
+            double numWithDecimal = 2.0/3.0;
+            double sale = .15;
+
+
+            NumberFormat money = NumberFormat.getCurrencyInstance();
+            NumberFormat number = NumberFormat.getIntegerInstance();
+            NumberFormat decimal= NumberFormat.getNumberInstance();
+            NumberFormat percent= NumberFormat.getPercentInstance();
+
+            System.out.println(money.format(dollars));
+            System.out.println(number.format(num));
+            System.out.println(decimal.format(numWithDecimal));
+            System.out.println(percent.format(sale));
+}
+}
